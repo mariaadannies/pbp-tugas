2bfd78d1b1789e30b19b4ee14f7bf9b243151458
mariaadannies <mariaadannies26@gmail.com>
2022-09-10 19:54:31 +0700

    deployment

--- katalog/views.py
+++ katalog/views.py
@@ -3,9 +3,11 @@ from katalog.models import CatalogItem
 
 # TODO: Create your views here.
 def show_CatalogItem(request):
-    return render(request, "katalog.html",context)
+    return render(request, "katalog.html", context)
 
 data_katalog = CatalogItem.objects.all()
 context = {
-    'list_katalog': data_katalog
+    'list_katalog': data_katalog,
+    'name': "Maria Aurellia",
+    'id': "2106750862",
 }
