81b8f00689dbb8f93f92a5a798157d170d88e1e0
mariaadannies <mariaadannies26@gmail.com>
2022-09-10 18:16:59 +0700

    routing

--- katalog/urls.py
+++ katalog/urls.py
@@ -1,1 +1,9 @@
 # TODO: Implement Routings Here
+from django.urls import path
+from katalog.views import show_CatalogItem
+
+app_name = 'katalog'
+
+urlpatterns = [
+    path('', show_CatalogItem, name='show_CatalogItem'),
+]   
--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
 # TODO: Create your views here.
+def show_CatalogItem(request):
+    return render(request, "katalog.html",context)
+
+data_katalog = CatalogItem.objects.all()
+context = {
+    'list_katalog': data_katalog
+}
